2bc4f52b93c281c4947a04853f447742aa689da3
samiul-alim12 <nawabitsamiul@gmail.com>
2025-07-16 14:24:52 +0600

    create a post and get api for handle useUserRole in cient side

--- index.js
+++ index.js
@@ -20,13 +20,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const database = client.db("dailyMarketDB");
+    const usersCollection = database.collection("users");
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email });
+
+      res.send({ role: user?.role || "user" });
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const existingUser = await usersCollection.findOne({ email: user.email });
+
+      if (existingUser) {
+        return res.status(200).send({ message: "User already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
